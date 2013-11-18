be043ca7ed5fbbe489be0922b3473c8bfb079142
Riku Lindblad <riku.lindblad@gmail.com>
2013-11-18 15:35:27 +0200

    Remove unused pychecker initialization

--- init.d/50-python.el
+++ init.d/50-python.el
@@ -1,11 +1,6 @@
 ; Indent python by 4 spaces
 (setq py-indent-offset 4)
 
-; use pychecker.sh for quality test
-(setq py-pychecker-command "pychecker.sh"
-      py-pychecker-command-args (quote (""))
-      python-check-command "pychecker.sh")
-
 (add-hook 'python-mode-hook
   #'(lambda ()
       ;; (define-key python-mode-map "\C-m" 'newline-and-indent)
@@ -13,7 +8,5 @@
       ;; (define-key python-mode-map (kbd "C-c C-:") 'python-indent-shift-right)
       ;; (define-key python-mode-map (kbd "C-c C-c") 'comment-region)
       ;; (define-key python-mode-map (kbd "C-c C-u") 'uncomment-region)
-      ;; (define-key python-mode-map (kbd "C-c C-n") 'flymake-goto-next-error)
-      ;; (define-key python-mode-map (kbd "C-c C-p") 'flymake-goto-prev-error)
       (subword-mode)
       (which-function-mode)))
