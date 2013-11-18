31f29fb47cf2392f3317cb5f889bc14b34faa517
Riku Lindblad <riku.lindblad@gmail.com>
2013-11-18 15:34:28 +0200

    Add MELPA packages to package.el configuration

---
+++ init.d/03-package.el
@@ -0,0 +1,6 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
