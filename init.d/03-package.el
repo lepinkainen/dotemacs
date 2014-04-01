f2e141a5868eb82a2d943a25c2fc2f8a3721d04b
Riku Lindblad <riku.lindblad@gmail.com>
2014-04-01 15:55:28 +0300

    Add marmelade to repos

--- init.d/03-package.el
+++ init.d/03-package.el
@@ -1,6 +1,9 @@
+(require 'cl)
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
