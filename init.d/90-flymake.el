53d9b5b9206a9799f050386c85efb43e10c400f5
Riku Lindblad <riku.lindblad@gmail.com>
2014-04-01 15:55:56 +0300

    use pycheckers method

--- init.d/90-flymake.el
+++ init.d/90-flymake.el
@@ -1,13 +1,13 @@
 (when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
+  (defun flymake-pycheckers-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
                        'flymake-create-temp-inplace))
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
+      (list "pycheckers" (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
+               '("\\.py\\'" flymake-pycheckers-init)))
 
 (delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
 
