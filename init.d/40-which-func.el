2d48cd8d35c652891943bc31266218ff092fa800
Riku Lindblad <riku.lindblad@gmail.com>
2014-04-11 13:03:28 +0300

    Add option for header line to which-func

--- init.d/40-which-func.el
+++ init.d/40-which-func.el
@@ -3,3 +3,11 @@
 (add-to-list 'which-func-modes 'org-mode)
 (which-function-mode t)
 (set-face-attribute 'which-func nil :foreground "Orange")
+
+;; Which-func to header line
+;(setq-default header-line-format
+;              '((which-func-mode ("" which-func-format " "))))
+;(setq mode-line-misc-info
+;            ;; We remove Which Function Mode from the mode line, because it's mostly
+;            ;; invisible here anyway.
+;            (assq-delete-all 'which-func-mode mode-line-misc-info))
