25557f66c1184414fdcda36cc8280473f550084b
Riku Lindblad <riku.lindblad@gmail.com>
2016-01-22 01:39:31 +0200

    Tweaks to backup and history

--- init.d/01-backup.el
+++ init.d/01-backup.el
@@ -5,9 +5,10 @@
 ;;; Code:
 (setq backup-directory-alist '(("." . "~/.emacs-backups"))) ; stop leaving backup~ turds scattered everywhere
 (setq backup-by-copying t
-      delete-old-versions t
+      delete-old-versions -1
       kept-new-versions 6
       kept-old-versions 2
-      version-control t)
+      version-control t
+      vc-make-backup-files t)
 
 ;;; 01-backup.el ends here
---
+++ init.d/02-history.el
@@ -0,0 +1,15 @@
+;;; history --- History settings
+;;; Commentary:
+
+;;; Code:
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
+;;; 02-history.el ends here
