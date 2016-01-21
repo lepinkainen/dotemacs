10639fd8317ec1844456440358904e62863ca357
Riku Lindblad <riku.lindblad@gmail.com>
2016-01-22 01:39:09 +0200

    Use smart-mode-line instead

--- init.d/50-modeline.el
+++ init.d/50-modeline.el
@@ -2,30 +2,32 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq-default mode-line-format
-      (list "["
-            'mode-line-client
-            "]"
-            ; Encoding
-            'mode-line-mule-info
-            ; */% indicators if the file has been modified
-            'mode-line-modified
-            "  "
-            ; line, column, file %
-            'mode-line-position
-            "| "
-            ; the name of the buffer (i.e. filename)
-            ; note this gets automatically highlighted
-            'mode-line-buffer-identification
-            "  "
-            'vc-mode
-            ; major and minor modes in effect
-            'mode-line-modes
-            ; if which-func-mode is in effect, display which
-            ; function we are currently in.
-            '(which-func-mode ("" which-func-format "--"))
-            "-%-"
-            )
-)
+(use-package smart-mode-line)
+
+;; (setq-default mode-line-format
+;;       (list "["
+;;             'mode-line-client
+;;             "]"
+;;             ; Encoding
+;;             'mode-line-mule-info
+;;             ; */% indicators if the file has been modified
+;;             'mode-line-modified
+;;             "  "
+;;             ; line, column, file %
+;;             'mode-line-position
+;;             "| "
+;;             ; the name of the buffer (i.e. filename)
+;;             ; note this gets automatically highlighted
+;;             'mode-line-buffer-identification
+;;             "  "
+;;             'vc-mode
+;;             ; major and minor modes in effect
+;;             'mode-line-modes
+;;             ; if which-func-mode is in effect, display which
+;;             ; function we are currently in.
+;;             '(which-func-mode ("" which-func-format "--"))
+;;             "-%-"
+;;             )
+;; )
 
 ;;; 50-modeline.el ends here
